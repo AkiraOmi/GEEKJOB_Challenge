d2abab01a46b18c3169ce5b6c2c345c7ecf1c5fd
DESKTOP-LT7VRD5\guest1Day <omi.akira.contact@gmail.com>
2017-05-19 14:09:13 +0900

    クラスの継承の課題を提出します。

---
+++ CampChallenge_java/005.オブジェクト指向１/Java_ClassExtend.java
@@ -0,0 +1,18 @@
+package challenge002;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Java_ClassExtend extends Java_ClassUse{
+    public void clear(){
+        n1 = 0;
+        n2 = 0;
+    }
+}
