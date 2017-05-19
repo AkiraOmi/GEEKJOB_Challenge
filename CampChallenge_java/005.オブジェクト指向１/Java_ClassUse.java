4497a4bbfe968f04498210707da4af8621b65c6e
DESKTOP-LT7VRD5\guest1Day <omi.akira.contact@gmail.com>
2017-05-19 13:53:00 +0900

    クラスの生成の課題を提出します

---
+++ CampChallenge_java/005.オブジェクト指向１/Java_ClassUse.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Java_ClassUse {
+    int n1;
+    int n2;
+    
+    public void setNumber(){
+        n1 = 1;
+        n2 = 2;
+    }
+    
+    public void printNumber(){
+        System.out.println("n1は" + n1 + "でn2は" + n2 + "です\n");
+    }
+}
